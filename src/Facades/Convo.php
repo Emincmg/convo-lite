847a815de7a2f94e03c15c5563336187cd941f0c
Emincmg <emin-comoglu@hotmail.com>
2024-11-18 11:06:18 +0300

    phpdoc documentation facade fix

--- src/Facades/Convo.php
+++ src/Facades/Convo.php
@@ -9,11 +9,19 @@ use Illuminate\Support\Facades\Facade;
  *
  * @package Symfony\App\Facade
  *
- * @method ConversationQuery create()
+ * @method static \Illuminate\Support\Collection createConversation(int $creatorId, array|int $receiverIds, ?string $title = null)
+ * @method static void addParticipators(\Conversation|int $conversation, int|array $users)
+ * @method static void removeParticipators(\Conversation|int $conversation, int|array $users)
+ * @method static void markConversationStatus(\Conversation|int $conversation, string $param)
+ * @method static \Conversation getConversationById(int $id)
+ * @method static \Illuminate\Support\Collection getConversationAttachments(\Conversation|int $conversation)
+ * @method static \Conversation|null getConversationByTitle(string $title)
+ * @method static \Illuminate\Support\Collection getMessagesByConversation(\Conversation $conversation)
+ * @method static \Message sendMessage(\Conversation|int $conversation, mixed $user, string $messageContent, \Illuminate\Http\UploadedFile|array|null $file = null)
  */
 class Convo extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'convo';
     }
