fa71c3cb4f24832cf96282d3f298cc18492c4969
Emincmg <emin-comoglu@hotmail.com>
2024-11-18 12:13:57 +0300

    phpdoc documentation facade fix

--- src/Traits/GetsUserInstance.php
+++ src/Traits/GetsUserInstance.php
@@ -5,13 +5,7 @@ use Emincmg\ConvoLite\Http\Controllers\Models\Conversation;
 
 trait GetsUserInstance
 {
-    /**
-     * Retrieve the user instance based on the provided user ID.
-     *
-     * @param int $userId The ID of the user.
-     * @return User|null The user instance if found, or null if not found.
-     */
-    private static function getUserInstance(int $userId): ?User
+    private static function getUserInstance(int $userId)
     {
         return resolve(config('convo-lite.user_model'))->find($userId);
     }
