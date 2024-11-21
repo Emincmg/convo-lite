9f13a94f0661b0c66cc88d5f7243488a2d9d91e7
Emincmg <emin-comoglu@hotmail.com>
2024-11-21 16:07:06 +0300

    broadcasting with read by relationship

--- src/Listeners/SendMessageNotification.php
+++ src/Listeners/SendMessageNotification.php
@@ -9,7 +9,7 @@ class SendMessageNotification
 {
     public function handle(MessageSent $event)
     {
-        if (config('convo-lite.send_message_notifications')){
+        if (config('convo-lite.send_message_notifications') === true) {
             $receivers = $event->message->receivers();
             foreach ($receivers as $receiver){
                 $receiver->notify(new NewMessageReceived($event->message));
